db4ac1baac8684be8a8214164f6791dd08e2fd16
William Connelly <williamconnelly.dev@gmail.com>
2018-06-13 23:33:22 -0700

    Added routes

--- index.js
+++ index.js
@@ -8,16 +8,51 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, "static")));
 app.set("view engine", "ejs");
-app.use(bp.urlencoded({extended: false}));
-app.use("ejsLayouts");
+app.use(bp.urlencoded({extended: true}));
+app.use(ejsLayouts);
 
 // GET / - Returns all games
-app.get("/", function(req, res) {
+app.get("/games", function(req, res) {
 	var games = fs.readFileSync("./data.json");
 	games = JSON.parse(games);
 	res.json(games);
 });
 
+// POST / - Adds a new game
+app.post("/games", function(req, res) {
+	var games = fs.readFileSync("./data.json");
+	games = JSON.parse(games);
+	games.push({name: req.body.name, rating: req.body.rating});
+	fs.writeFileSync("./data.json", JSON.stringify(games));
+	res.json(games);
+});
 
+// TODO GET /games/:id - gets one game
+app.get("/games/:id", function(req, res) {
+	var games = fs.readFileSync("./data.json");
+	games = JSON.parse(games);
+	var gameIndex = req.params.id;
+	res.json(games[gameIndex]);
+});
+
+// TODO PUT /games/:id/ - updates one game
+app.put("/games/:id", function(req, res) {
+	var games = fs.readFileSync("./data.json");
+	games = JSON.parse(games);
+	var gameIndex = req.params.id;
+	games[gameIndex] = {name: req.body.name, rating: req.body.rating};
+	fs.writeFileSync("./data.json", JSON.stringify(games));
+	res.json(games);
+});
+
+// TODO DELETE /games/:id - deletes one game
+app.delete("/games/:id", function(req, res) {
+	var games = fs.readFileSync("./data.json");
+	games = JSON.parse(games);
+	var gameIndex = req.params.id;
+	games.splice(gameIndex, 1);
+	fs.writeFileSync("./data.json", JSON.stringify(games));
+	res.json(games);
+});
 
 app.listen(3000);
